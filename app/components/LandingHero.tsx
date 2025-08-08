5b4e3a7db6c7c044d512747e9c546cb0d628a5a5
bekzod1337 <rahimjonovazarina@gmail.com>
2025-08-08 22:27:35 +0500

    main text update

--- app/components/LandingHero.tsx
+++ app/components/LandingHero.tsx
@@ -21,27 +21,32 @@ export default function LandingHero() {
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.4 }}
-          className="absolute top-[65px] left-[486px] text-[24px] text-white/75 font-black tracking-[0.3em] uppercase leading-[75px]"
+          className="absolute top-[65px] left-[486px] text-[35px] text-white/75 font-black tracking-[0.3em] uppercase leading-[75px]"
         >
           I AM
         </motion.div>
 
         {/* AURA */}
         <motion.h1
-          initial={{ opacity: 0, y: 60 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.2, delay: 0.6 }}
-          className="absolute top-50 left-0 w-full text-white font-black text-[188px] tracking-[0.05em] uppercase leading-[70px] text-center items-center"
-        >
-          BEKKZOD
-        </motion.h1>
+  initial={{ opacity: 0, y: 60 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 1.2, delay: 0.6 }}
+  className="absolute top-50 left-0 w-full font-black text-[188px] tracking-[0.05em] uppercase leading-[70px] text-center items-center"
+  style={{
+    WebkitTextStroke: '2px white',
+    color: 'transparent',
+  }}
+>
+  BEKKZOD
+</motion.h1>
+
 
         {/* AN Environmentalist */}
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.9 }}
-          className="absolute top-[396px] left-[764px] text-[24px] text-white/75 font-black tracking-[0.3em] uppercase leading-[40px]"
+          className="absolute top-[396px] left-[764px] text-[35px] text-white/75 font-black tracking-[0.3em] uppercase leading-[40px]"
         >
           WEB <br /> Developer
         </motion.div>
